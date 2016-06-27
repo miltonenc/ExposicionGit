43105d0451da11146412d4a870e4d8e373eeec72
Oswald Moreno <oswald.akira@gmail.com>
2016-06-27 18:05:39 -0400

    Clase que solo existe en Develop

---
+++ ExposicionGit/app/src/main/java/iso600/unapec/com/exposiciongit/Estudent.java
@@ -0,0 +1,19 @@
+package iso600.unapec.com.exposiciongit;
+
+/**
+ * Created by Oswald on 6/27/16.
+ */
+public class Estudent {
+    private String name;
+
+    public Estudent() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
